b17d53e78619f8cd02b4fd908783509373012ea2
Andrew Lofts <a_lofts@hotmail.ca>
2018-07-27 10:52:42 -0400

    add file create and save to mimic oppni

--- mars.py
+++ mars.py
@@ -1,2 +1,5 @@
 #CHANGED THIS TO HELP WITH DOCKER TESTING
 print('HELLO FROM INIDE THE DOCKER, this would be oppni python running');
+f = open("Task_Complete_CBRAIN.txt","w+");
+f.write("OPPNI STATUS TEST FILE, TO TRIGGER CBRAIN SUCCESS");
+f.close();
